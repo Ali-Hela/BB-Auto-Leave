2e5ce97a28f50b4ef38992e14ed0948eea13e443
Ali <52052529+Ali-Hela@users.noreply.github.com>
2022-03-11 16:44:38 +0300

    Add files via upload

---
+++ BBLeave.js
@@ -0,0 +1,52 @@
+document.onload = setTimeout(function () {
+    let leaveTime;
+    let timeStr;
+    let hour = 0;
+    let minute = 0;
+    let now = new Date();
+
+    let myDiv = document.getElementsByClassName("scroll-content")[0];
+    let button = document.createElement("BUTTON");
+    button.innerHTML = "Set Timer";
+    myDiv.appendChild(button);
+
+    //Styling
+    button.style.border = "solid";
+    button.style.borderColor = "RGB(112,37,126)";
+    button.style.backgroundColor = "RGB(12,12,13)";
+    button.style.padding = "5px";
+    button.style.borderRadius = "5";
+    button.style.marginTop = "15px"
+
+
+    button.onclick = function () {
+        leaveTime = prompt("Leave class at:\n(Example: 2:15)")
+        refactor();
+        start();
+    }
+
+    // Refactor input regarding if with or without minutes
+    function refactor() {
+        if (leaveTime.includes(":")) {
+            timeStr = leaveTime.split(":");
+            hour = parseInt(timeStr[0]);
+            minute = parseInt(timeStr[1]);
+        }
+        else {
+            hour = parseInt(leaveTime);
+        }
+    }
+
+    // 12 hours to 24 hours then start timer
+    function start() {
+        button.innerHTML = "Leaving at: " + hour.toString().padStart(2, "0") + ":" + minute.toString().padStart(2, "0");
+        if (now.getHours() >= 12)
+            hour += 12;
+
+        var msTillTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), hour, minute, 0, 0) - now;
+        if (msTillTime < 0) {
+            msTillTime += 86400000;
+        }
+        setTimeout(() => { window.location.href = "../" }, msTillTime)
+    }
+}, 7000);
